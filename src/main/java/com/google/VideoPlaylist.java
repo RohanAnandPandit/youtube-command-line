86a929a0c7aaf298b5da1a492bdeeb1f4d1c4383
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-08-04 16:40:24 +0100

    Implement VideoPlaylist to keep track of videos

--- src/main/java/com/google/VideoPlaylist.java
+++ src/main/java/com/google/VideoPlaylist.java
@@ -1,6 +1,39 @@
 package com.google;
 
+import java.util.*;
+
 /** A class used to represent a Playlist */
 class VideoPlaylist {
 
+    private final String name;
+    private final Set<String> videos;
+
+
+    VideoPlaylist(String name) {
+        this.name = name;
+        this.videos = new LinkedHashSet<>();
+    }
+
+    public boolean containsVideo(String videoId) {
+        return videos.contains(videoId);
+    }
+
+    public void addVideo(String videoId) {
+        videos.add(videoId);
+    }
+
+    public void removeVideo(String videoId) {
+        videos.remove(videoId);
+    }
+    public Set<String> getVideosIds() {
+        return videos;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void removeAllVideos() {
+        videos.clear();
+    }
 }
